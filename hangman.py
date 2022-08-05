a60d382d1cb9c9de9aa99ff82ec69fbd83372496
sanju <sanjusk08gmail.com>
2022-08-05 12:59:19 +0530

    removed bunch of stuff

--- hangman.py
+++ hangman.py
@@ -26,31 +26,5 @@ def mask_word(secret_word, guessed_letters):
     return masked_word
 
 
-def Hangman:
-    def __init__(self):
-        self.chances = 6
-        self.secret = get_random_word()
-        self.guessed_letters = {}
-        self.masked_word = '-' * len(self.secret)
-   
-    def game_won():
-        if self.secret == self.masked_word:
-            return True
-        return False
-            
-    def run():
-        print(self.secret + '\t' + self.chances)
-        while self.turn == 0:
-            guess = input('Enter you guess:').lower()
-            if len(guess) != 1 and not guess.isalpha():
-                print('You need to enter a single character') 
-                continue
-            self.guessed_letters.add(guess)
-            self.masked_word = mask_word(self.secret, list(guess))
-            if self.game_won():
-                print('You won')
-                return
-            self.chances += 1
-
 if __name__ == "__main__":
     pass
