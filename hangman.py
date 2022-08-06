1a8b25f2fd8dc434088485de950448c6ae211834
sanju <sanjusk08gmail.com>
2022-08-06 17:37:47 +0530

    green: all tests passing

--- hangman.py
+++ hangman.py
@@ -1,5 +1,11 @@
 import random
 
+WON = 0
+LOST = 1
+ALREADY_GUESSED = 2
+GOOD_GUESS = 3
+BAD_GUESS = 4
+
 
 def get_random_word(path="/usr/share/dict/words"):
     words = []
@@ -26,5 +32,58 @@ def mask_word(secret_word, guessed_letters):
     return masked_word
 
 
+def get_status(secret_word, guessed_letters, turns_left, result):
+    if result in (WON, LOST):
+        place_holder = 'won 👍' if result == WON else 'lost 👎'
+        return f"""You {place_holder}
+    The secret word is {secret_word}"""
+
+    ret = f"""turns_left: {turns_left}
+    guessed_letters: {" ".join(guessed_letters)}
+    {mask_word(secret_word, guessed_letters)}
+    """
+
+    if result == ALREADY_GUESSED:
+        ret = "You already guessed that letter\n" + ret
+    return ret
+
+
+def process_turn(current_guess, secret_word, guessed_letters, turns_left):
+    if current_guess in guessed_letters:
+        return turns_left, guessed_letters, ALREADY_GUESSED
+    if secret_word == mask_word(secret_word, guessed_letters + [current_guess, ]):
+        return turns_left, guessed_letters, WON
+    if turns_left == 1:
+        return turns_left, guessed_letters, LOST
+
+    guessed_letters = guessed_letters + [current_guess, ]
+
+    if current_guess not in secret_word:
+        turns_left -= 1
+        result = BAD_GUESS
+    else:
+        result = GOOD_GUESS
+    return turns_left, guessed_letters, result
+
+
+def main():
+    secret_word = get_random_word()
+    turns_left = 7
+    guessed_letters = []
+    print(secret_word)
+    while True:
+        print(get_status(secret_word, guessed_letters, turns_left, GOOD_GUESS))
+        current_guess = input("guess a letter:")
+        turns_left, guessed_letters, result = process_turn(
+            current_guess, secret_word, guessed_letters, turns_left
+        )
+        if result == WON:
+            print(get_status(secret_word, guessed_letters, turns_left, result))
+            break
+        if result == LOST:
+            print(get_status(secret_word, guessed_letters, turns_left, result))
+            break
+        
+
 if __name__ == "__main__":
-    pass
+    main()
--- test_hangman.py
+++ test_hangman.py
@@ -49,28 +49,134 @@ def test_mask_word_no_guesses():
         assert hangman.mask_word(i, []) == len(i) * "-"
 
 
-def test_mask_word_invalid_guesses():
+def test_mark_word_invalid_guesses():
     for i in ["police", "elephant", "it", "foo"]:
         assert hangman.mask_word(i, ["x", "q"]) == len(i) * "-"
 
 
 def test_mask_word_single_guesses():
-    assert hangman.mask_word('football', ['b']) == '----b---'
-    assert hangman.mask_word('orange', ['o']) == 'o-----'
-    assert hangman.mask_word('basketball', ['k']) == '---k------'
+    assert hangman.mask_word("football", ["b"]) == "----b---"
+    assert hangman.mask_word("orange", ["o"]) == "o-----"
+    assert hangman.mask_word("basketball", ["k"]) == "---k------"
 
 
-def test_mask_multiple_guess():
+def test_mask_word_repeated_char():
     secret_word = "elephant"
     guessed_letters = ["e", "p"]
     assert hangman.mask_word(secret_word, guessed_letters) == "e-ep----"
-    guessed_letters.append("z")
-    assert hangman.mask_word(secret_word, guessed_letters) == "e-ep----"
-    assert hangman.mask_word('madam', ['m', 'a', 'd']) == 'madam'
-    assert hangman.mask_word('madam', ['m', 'a', 'd', 'm']) == 'madam'
-    assert hangman.mask_word('madam', ['m', 'a', 'd', 'm', '']) == 'madam'
-    
+
+    assert hangman.mask_word("madam", ["m", "a", "d"]) == "madam"
+
+
+def test_get_status_when_lost():
+    secret_word = 'amazon'
+    guessed_letters = None
+    turns_left = None
+    result = hangman.LOST
+    status = """You lost 👎
+    The secret word is amazon"""
+    assert hangman.get_status(secret_word, guessed_letters, turns_left, result) == status
+
+
+def test_get_status_when_won():
+    secret_word = 'amazon'
+    guessed_letters = None
+    turns_left = None
+    result = hangman.WON
+    status = """You won 👍
+    The secret word is amazon"""
+    assert hangman.get_status(secret_word, guessed_letters, turns_left, result) == status
+
+
+def test_get_status_when_good_guess():
+    secret_word = 'monkey'
+    guessed_letters = list('mon')
+    turns_left = 3
+    result = hangman.GOOD_GUESS
+
+    status = f"""turns_left: {turns_left}
+    guessed_letters: m o n
+    mon---
+    """
+    assert hangman.get_status(secret_word, guessed_letters, turns_left, result) == status
+
+
+def test_get_status_when_bad_guess():
+    secret_word = 'monkey'
+    guessed_letters = list('mon')
+    turns_left = 3
+    result = hangman.BAD_GUESS
+
+    status = f"""turns_left: 3
+    guessed_letters: m o n
+    mon---
+    """
+    assert hangman.get_status(secret_word, guessed_letters, turns_left, result) == status
+
+
+def test_get_status_when_already_guessed():
+    secret_word = 'monkey'
+    guessed_letters = list('mon')
+    turns_left = 3
+    result = hangman.ALREADY_GUESSED
+
+    status = f"""You already guessed that letter\nturns_left: 3
+    guessed_letters: m o n
+    mon---
+    """
+    assert hangman.get_status(secret_word, guessed_letters, turns_left, result) == status
+
+def test_process_turn_already_guessed():
+    current_guess = "s"
+    secret_word = "secret"
+    guessed_letters = list("se")
+    turns_left = 4
+
+    assert hangman.process_turn(
+        current_guess, secret_word, guessed_letters, turns_left
+    ) == (turns_left, guessed_letters, hangman.ALREADY_GUESSED)
+
+
+def test_process_turn_game_won():
+    current_guess = "a"
+    secret_word = "amazing"
+    guessed_letters = list("mzing")
+    turns_left = 4
+    assert hangman.process_turn(
+        current_guess, secret_word, guessed_letters, turns_left
+    ) == (turns_left, guessed_letters, hangman.WON)
+
+
+def test_process_turn_game_lost():
+    current_guess = 'b'
+    secret_word = 'amazing'
+    guessed_letters = list('mzing')
+    turns_left = 1
+
+    assert hangman.process_turn(
+        current_guess, secret_word, guessed_letters, turns_left
+    ) == (turns_left, guessed_letters, hangman.LOST)
+
+
+def test_process_turn_bad_guess():
+    current_guess = 'x'
+    secret_word = 'wallaby'
+    guessed_letters = list('lla')
+    turns_left = 2
+    assert hangman.process_turn(
+        current_guess, secret_word, guessed_letters, turns_left
+    ) == (turns_left - 1, guessed_letters + ['x', ], hangman.BAD_GUESS)
+
+
+def test_process_turn_good_guess():
+    current_guess = 'b'
+    secret_word = 'wallaby'
+    guessed_letters = list('lla')
+    turns_left = 2
+    assert hangman.process_turn(
+        current_guess, secret_word, guessed_letters, turns_left
+    ) == (turns_left, guessed_letters + ['b', ], hangman.GOOD_GUESS)
 
 # RED - Implement a test that will fail
 # GREEN - Make the test pass
-# refactoR - Adjust the code so that all tests pass and code is improved
+# REFACTOR - Adjust the code so that all tests pass and code is improved
